e6b06d5134a4399b981c2a137bd0904d42cbb79f
parkoon <jh.park@qualson.com>
2020-04-04 11:15:02 +0900

    add app layout component

---
+++ src/components/oragnisms/AppLayout.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import AppNav, { AppBarHeight } from '../molecules/AppBar'
+import styled from 'styled-components'
+import { theme } from '../../Theme'
+
+const { unit } = theme
+
+const AppLayoutWrapper = styled.div`
+  margin-top: ${AppBarHeight}px;
+`
+const AppLayoutBody = styled.div`
+  padding: ${unit * 6}px;
+`
+
+function AppLayout(children) {
+  return (
+    <AppLayoutWrapper>
+      <AppNav />
+
+      <AppLayoutBody>{children}</AppLayoutBody>
+    </AppLayoutWrapper>
+  )
+}
+
+export default AppLayout
