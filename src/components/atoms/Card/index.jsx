e5da7309e3c4da825efa8b2d121a70fd121afe52
parkoon <jh.park@qualson.com>
2020-04-04 11:36:03 +0900

    update card props

--- src/components/atoms/Card/index.jsx
+++ src/components/atoms/Card/index.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import styled from 'styled-components'
 import { theme } from '../../../Theme'
+import Spacing from '../Spacing'
 
 const StyledCardWrapper = styled.div`
   display: flex;
+  flex-direction: column;
   overflow: hidden;
   margin-bottom: ${theme.unit * 4}px;
   background: ${theme.color.white};
   box-shadow: 0 0 1px rgba(0, 0, 0, 0.5);
-  border-radius: ${theme.unit}px;
 `
 
-function Card({ children }) {
-  return <StyledCardWrapper>{children}</StyledCardWrapper>
+function Card({ children, ...spacingProps }) {
+  return (
+    <StyledCardWrapper>
+      <Spacing {...spacingProps}>{children}</Spacing>
+    </StyledCardWrapper>
+  )
 }
 
 export default Card
