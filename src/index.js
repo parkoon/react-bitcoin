c39aff32496d1b0f812729187ccc8f8d4f480eae
parkoon <jh.park@qualson.com>
2020-04-04 10:21:12 +0900

    폴더 구조 변경

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import CoinApp from './CoinApp'
 
 import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CoinApp />
   </React.StrictMode>,
   document.getElementById('root')
 )
